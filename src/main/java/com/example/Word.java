c62fd295cdf0ce7a775ec039445e3917b9339d16
marqnpl <marqnpl@gmail.com>
2015-12-07 06:52:15 +0100

    Improvement word entity

--- src/main/java/com/example/Word.java
+++ src/main/java/com/example/Word.java
@@ -1,10 +1,13 @@
 package com.example;
 
 
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 
 @Entity
@@ -16,6 +19,44 @@ public class Word {
 
     public String first;
     public String second;
+    public Integer refresh;
+    public Integer wiem;
+    public Integer niewiem;
+
+    @UpdateTimestamp
+    public Date createdData;
+
+    public Integer getRefresh() {
+        return refresh;
+    }
+
+    public void setRefresh(Integer refresh) {
+        this.refresh = refresh;
+    }
+
+    public Integer getWiem() {
+        return wiem;
+    }
+
+    public void setWiem(Integer wiem) {
+        this.wiem = wiem;
+    }
+
+    public Integer getNiewiem() {
+        return niewiem;
+    }
+
+    public void setNiewiem(Integer niewiem) {
+        this.niewiem = niewiem;
+    }
+
+    public Date getCreatedData() {
+        return createdData;
+    }
+
+    public void setCreatedData(Date createdData) {
+        this.createdData = createdData;
+    }
 
     public void setId(Long id) {
         this.id = id;
